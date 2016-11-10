29f5f7d8b6001260c90f3d2dfa544fdd4a4dfcf6
ARUGA, Kazuki <kazuki.aruga@gmail.com>
2016-11-10 19:55:25 +0900

    テスト追加

--- src/test/java/io/github/kazuki_aruga/text_analyzer/FileNameTest.java
+++ src/test/java/io/github/kazuki_aruga/text_analyzer/FileNameTest.java
@@ -1,11 +1,12 @@
 package io.github.kazuki_aruga.text_analyzer;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,14 +15,18 @@ import org.junit.Test;
 public class FileNameTest {
 
 	@Test
-	public void test() throws IOException {
+	public void test() throws IOException, ParseException {
 
 		final Pattern pattern = Pattern.compile(
-				"^([0-9]{4})_([^_]+)_有価証券報告書第([0-9]{1,2})期（平成([0-9]{1,2})年([0-9]{1,2})月([0-9]{1,2})日-平成([0-9]{1,2})年([0-9]{1,2})月([0-9]{1,2})日）[.]txt$");
+				"^([0-9]{4})_[^_]+_有価証券報告書第[0-9]{1,2}期（(平成[0-9]{1,2}年[0-9]{1,2}月[0-9]{1,2}日)-(平成[0-9]{1,2}年[0-9]{1,2}月[0-9]{1,2}日)）[.]txt$");
 
-		final File dirfile = new File("C:\\Temp\\dir.txt");
+		final DateFormat japaseseFormat = new SimpleDateFormat("GGGGy年M月d日", new Locale("ja", "JP", "JP"));
+		final DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(dirfile), "MS932"))) {
+		// 和暦にフォーマットした現在日時を西暦にパースする
+		// 日付の妥当性をチェックするためsetLenient()にfalseを渡す
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(ClassLoader.getSystemResourceAsStream("filename-list.txt"), "MS932"))) {
 
 			for (String line = null; (line = reader.readLine()) != null;) {
 
@@ -29,7 +34,13 @@ public class FileNameTest {
 
 				if (matcher.matches()) {
 
-					System.out.println("OK: " + line);
+					final String compCode = matcher.group(1);
+					final String empDateFrom = matcher.group(2);
+					final String empDateTo = matcher.group(3);
+
+					System.out.print("OK: " + line);
+					System.out.print("Date From: " + df.format(japaseseFormat.parse(empDateFrom)));
+					System.out.println("Date To  : " + df.format(japaseseFormat.parse(empDateTo)));
 
 				} else {
 
