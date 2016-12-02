899b25a8bf577060f6f50f93489bdf12bafb9bab
ARUGA, Kazuki <kazuki.aruga@gmail.com>
2016-12-02 20:24:14 +0900

    エクスポートコマンドを追加

--- src/main/sql/export_import.cmd
+++ src/main/sql/export_import.cmd
@@ -1,2 +1,5 @@
 rem export
 mysqldbexport --server=root:password@localhost --export=DATA --bulk-insert --output-file=C:\Temp\data\text-analyzer.sql text-analyzer
+
+rem csv data export
+mysqldbexport --server=root:password@localhost --export=DATA --format=csv --file-per-table text-analyzer
